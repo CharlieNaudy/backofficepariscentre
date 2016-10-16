a4c14af35d9776e1ce4c0c8d9acc4c48eb446ea7
Charlie <charlie.naudy@gmail.com>
2016-10-16 16:54:50 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Meeting.create(
+	id_meeting: "Se la coller à République",
+	latitude: 48.86751,
+	longitude: 2.363777,
+	participants: "Paul, Pierre, Jacques",
+	date: DateTime.new(2016,12,31,23,59,59)
+	)
+Meeting.create(
+	id_meeting: "Tournée générale d'aspirine",
+	latitude: 48.852969,
+	longitude: 2.349873,
+	participants: "Jacqui Michel",
+	date: DateTime.new(2017,01,01,14,00,00)
+	)
