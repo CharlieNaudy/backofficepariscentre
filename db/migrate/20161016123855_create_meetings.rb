13fc00955137b7b6f077764a53dbd08a653bb115
Charlie <charlie.naudy@gmail.com>
2016-10-22 18:57:05 +0200

    pb meeting_id solved, users work, still need to find a way to add users to meetings ("participants" should become a list of users)

--- db/migrate/20161016123855_create_meetings.rb
+++ db/migrate/20161016123855_create_meetings.rb
@@ -1,7 +1,7 @@
 class CreateMeetings < ActiveRecord::Migration[5.0]
   def change
     create_table :meetings do |t|
-      t.string :id_meeting
+      t.string :nom
       t.float :latitude
       t.float :longitude
       t.string :participants
