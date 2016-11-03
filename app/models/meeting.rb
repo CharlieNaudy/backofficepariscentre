0e1254b34b00a2c5647e6cf503fc2b221882e1a6
Charlie <charlie.naudy@gmail.com>
2016-11-03 18:51:10 +0100

    fonction barycentre

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,20 @@
 class Meeting < ApplicationRecord
 	has_many :users
 
+
+
+  def self.calcul_barycentre
+    if self.users.present?
+    	a = Array.new
+    	self.users.each do |user|
+      		a.push(user)
+    	end
+    self.latitude = addressGeocoder::Calculations.geographic_center(a)[1]
+    self.longitude = addressGeocoder::Calculations.geographic_center(a)[2]
+  	end
+  end
+
+	before_save self.calcul_barycentre
 	geocoded_by :address
 	reverse_geocoded_by :latitude, :longitude
 	after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
